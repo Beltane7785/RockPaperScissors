3dac5e451c5b0545773f21db25880204bcbcefdf
Evan McDonald <evanmcdonald4025@gmail.com>
2024-04-15 06:47:21 -0500

    Added winner functionality and completed the project.

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -3,26 +3,46 @@ string computerChoice;
 int playerScore = 0;
 int computerScore = 0;
 
-while (playerScore < 2 && computerScore < 2)
+while (playerScore < 3 && computerScore < 3)
 {
     Console.WriteLine("Please choose Rock, Paper, or Scissors");
 
     playerOneChoice = Console.ReadLine();
 
-    Console.WriteLine(PlayerChoice(playerOneChoice));
-    Console.WriteLine(ComputerChoice());
-    playerScore++;
+    string playerChoice = SetPlayerChoice(playerOneChoice);
+    computerChoice = SetComputerChoice();
+
+    Console.WriteLine($"The Player's choice is {playerChoice}. The Computer's choice is {computerChoice}");
+
+    string winner = ChooseWinner(playerChoice, computerChoice);
+    
+    if (winner == "Player")
+    {
+        Console.WriteLine($"The Player is the winner! The score is {playerScore} to {computerScore}");
+    }
+
+    if (winner == "Computer")
+    {
+        Console.WriteLine($"The Computer is the winner! The score is {playerScore} to {computerScore}");
+    }
+
+    if (winner == "Draw")
+    {
+        Console.WriteLine($"It is a draw! The score is {playerScore} to {computerScore}");
+    }
 }
 
-Console.WriteLine("Please choose Rock, Paper, or Scissors");
+if (playerScore == 3)
+{
+    Console.WriteLine("The Player wins the match!");
+}
 
-playerOneChoice = Console.ReadLine();
+if (computerScore == 3)
+{
+    Console.WriteLine("The Computer wins the match!");
+}
 
-Console.WriteLine(PlayerChoice(playerOneChoice));
-Console.WriteLine(ComputerChoice());
-
-
-string PlayerChoice(string playerInput)
+string SetPlayerChoice(string playerInput)
 {
     string[] choices = ["Rock", "Paper", "Scissors"];
     string errorMessage = "You have not chosen a valid option, please choose again.";
@@ -37,14 +57,56 @@ string PlayerChoice(string playerInput)
         case "Scissors": return choices[2];
         default: return errorMessage;
     }
-    
 }
 
-string ComputerChoice()
+string SetComputerChoice()
 {
     string[] choices = ["Rock", "Paper", "Scissors"];
     Random random = new Random();
     int choiceIndex = random.Next(0, choices.Length);
-
     return choices[choiceIndex];
+}
+
+string ChooseWinner(string playerChoice, string computerChoice)
+{
+    string winner;
+
+    if (playerChoice == "Rock" && computerChoice == "Scissors")
+    {
+        playerScore++;
+        return winner = "Player";
+    }
+
+    if (playerChoice == "Scissors" && computerChoice == "Rock")
+    {
+        computerScore++;
+        return winner = "Computer";
+    }
+
+    if (playerChoice == "Rock" && computerChoice == "Paper")
+    {
+        computerScore++;
+        return winner = "Computer";
+    }
+
+    if (playerChoice == "Paper" && computerChoice == "Rock")
+    {
+        playerScore++;
+        return winner = "Player";
+    }
+
+    if (playerChoice == "Scissors" && computerChoice == "Paper")
+    {
+        playerScore++;
+        return winner = "Player";
+    }
+
+    if (playerChoice == "Paper" && computerChoice == "Scissors")
+    {
+        computerScore++;
+        return winner = "Computer";
+    }
+
+    return winner = "Draw";
+    
 }
